51284837fade6649fd450a2bc0805d8c0488ea4a
Cosmin Popa <cosmin7popa@gmail.com>
2024-10-17 12:49:04 +0300

    Make necessary changes
    Signed-off-by: Cosmin Popa <cosmin7popa@gmail.com>

---
+++ src/HelloWorld.java
@@ -0,0 +1,39 @@
+public class HelloWorld {
+    /**
+     * Print "Hello, World" to the terminal window.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+
+    /*
+    Exercitiul 3:
+        true
+        true
+        true
+        true
+        false
+        true
+        false
+        eroare
+        true
+        false
+     */
+
+    /*
+    Exercitiul 4:
+        100.25
+        100
+        100
+        100
+     */
+
+    /*
+    Exercitiul 5:
+        -5
+        0
+        40
+     */
+}
